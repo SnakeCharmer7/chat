e43f2ec80ee808aebbdebea2de945384aef836b0
SnakeCharmer7 <splocienniczak54@gmail.com>
2025-03-04 17:16:02 +0100

    First commit

---
+++ chat/forms.py
@@ -0,0 +1,70 @@
+from django import forms
+from django.contrib.auth.models import User
+import re
+from .models import *
+
+
+class ChatmessageCreateForm(forms.ModelForm):
+    class Meta:
+        model = GroupMessage
+        fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].label = ""
+        self.fields['text'].widget.attrs.update({
+            'placeholder': 'Add message ...', 
+            'class': 'form-control rounded-start', 
+            'autofocus': True
+        })
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, min_length=8, required=True)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username']
+        help_texts = {'username': None}
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if len(username) < 8:
+            raise forms.ValidationError("Username must be at least 8 characters long.")
+        if len(username) >= 20:
+            raise forms.ValidationError("Username must contain a maximum of 20 characters.")
+        if not re.match(r"^[a-zA-Z0-9_-]+$", username):
+            raise forms.ValidationError("Username can only contain letters, numbers, underscores, and hyphens.")
+
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+
+        if not re.search(r'[A-Z]', password):
+            raise forms.ValidationError("Password must contain at least one uppercase letter.")
+        if not re.search(r'[a-z]', password):
+            raise forms.ValidationError("Password must contain at least one lowercase letter.")
+        if not re.search(r'\d', password):
+            raise forms.ValidationError("Password must contain at least one number.")
+
+        return password
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password and password_confirm and password != password_confirm:
+            self.add_error('password_confirm', "Passwords must be the same.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
---
+++ chat/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.urls import reverse_lazy
+from .models import *
+from .forms import *
+
+
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatmessageCreateForm()
+
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message': message,
+                'user': request.user
+            }
+            return render(request, 'chat/partials/chat_message_p.html', context)
+
+    return render(request, "chat/chat.html", {'chat_messages': chat_messages, 'form': form})
+
+
+class SingUpView(generic.CreateView):
+    template_name = "registration/register.html"
+    success_url = reverse_lazy('login')
+    form_class = UserCreationForm
