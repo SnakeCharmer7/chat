77bce504f1ccd06b337b77568dd07b8d93d288b4
SnakeCharmer7 <splocienniczak54@gmail.com>
2025-03-12 22:08:38 +0100

    Changed requirements for Render

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for chat_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -15,6 +6,10 @@ from channels.auth import AuthMiddlewareStack
 from chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
+
+import django
+django.setup()
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
