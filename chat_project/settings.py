29f5fc7a97668f65a72d2f306a524a7906fc6b9d
SnakeCharmer7 <splocienniczak54@gmail.com>
2025-03-13 20:51:36 +0100

    Repair hosts

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -40,7 +40,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'chat-w3g7.onrender.com']
 
-CSRF_THRUSTED_ORIGINS = [ 'https://*.chat-w3g7.onrender.com' ]
+CSRF_THRUSTED_ORIGINS = [ 'https://chat-w3g7.onrender.com' ]
 
 # Application definition
 
