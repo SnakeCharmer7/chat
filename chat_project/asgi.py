5b6b8b4c539fb557b87ab26f0e575ab3a50e8183
SnakeCharmer7 <splocienniczak54@gmail.com>
2025-03-13 20:18:09 +0100

    Changed requirements for Render

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,14 +1,14 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from chat import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
-
-import django
-django.setup()
 
 
 application = ProtocolTypeRouter({
